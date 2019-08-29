6f89463d95ba92f7015959f9ddfd58181e8dd460
khalilhayek <khalilhayek@users.noreply.github.com>
2019-08-29 15:47:10 -0400

    Update letter-frequencies.rb

--- letter-frequencies.rb
+++ letter-frequencies.rb
@@ -3,15 +3,7 @@ class Freq
 		@frequencies = {}
 		('a'..'z').each { |c| @frequencies[c] = 0 }
 	end
-	
-	def process str
-		str.downcase.each_char { |c| @frequencies[c] += 1 if c.between? 'a','z' }
-	end
-	
-	def show
-		p @frequencies.sort_by { |_,freq| -freq }
-	end
-	
+
 	def run
 		puts 'Type some text:'
 		while ( input = gets.chomp ) =~ /^q$/
@@ -20,7 +12,17 @@ class Freq
 		end
 		show
 	end
+	
+private
+	def process str
+		str.downcase.each_char { |c| @frequencies[c] += 1 if c.between? 'a','z' }
+	end
+	
+	def show
+		p @frequencies.sort_by { |_,freq| -freq }
+	end
 end
+
 
 if __FILE__ == $PROGRAM_NAME
 	freqs = Freq.new
