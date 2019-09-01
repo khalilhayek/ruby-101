46f1983248cb93a187971c038a0d04e25fb827da
khalilhayek <khalilhayek@users.noreply.github.com>
2019-08-31 20:22:32 -0400

    Update 02-variables.rb

--- 02-variables.rb
+++ 02-variables.rb
@@ -1,15 +1,16 @@
 # In Ruby, you can can stuff things like 123 or 'hello' 
-# into boxes called "variables".
+# into containers called "variables".
+# This is called "assigning a value to a variable".
 
 name = 'Bunbuns'
 color = 'gray'
 age = 2
 likes_carrots = true
 
-# You can also do this on one line.
+# You can also do multiple assignments on one line.
 name, color, age, likes_carrots = 'Bunbuns', 'gray', 2, true
 
-# You can change the values of variables. 
+# You can change the value of a variable. 
 # So, a year later, you can do:
 age = age + 1
 # Or even: 
@@ -30,11 +31,12 @@ bunnys_loves_carrots_100percent = true
 bunnyLovesCarrots100Percent = true
 
 # In Ruby, the preferred way to name long variable names 
-# is with an underscore:
+# is with an underscore, like this.
 my_home_address = '1, Carrot Lane'
 
 # Variables can contain arrays and ranges too.
-bunnys_lucky_numbers = [2,4,8]
+bunnys_lucky_numbers = [2,3,4,5,6,7,8]
+range_of_lucky_numbers = (1..10)
 
 
 
